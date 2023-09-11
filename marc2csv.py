d291951b1eb2271fe1bf65be4154b82de9451c0b
Pablo <pablo@pop-os.localdomain>
2023-09-11 17:56:08 -0300

    Handle bad records and signals like SIGPIPE

--- marc2csv.py
+++ marc2csv.py
@@ -1,8 +1,9 @@
 # Simple marc2csv script.
 
 import csv
-# import user
 import sys
+from signal import signal, SIGPIPE, SIG_DFL
+signal(SIGPIPE, SIG_DFL)
 
 from pymarc import MARCReader
 
@@ -11,7 +12,6 @@ if len(sys.argv) > 1:
     filepath = sys.argv[1]
 
 try:
-    # reader = MARCReader(open(filepath))
     reader = MARCReader(open(filepath, 'rb'), to_unicode=True)
 except IOError:
     print('cannot open "%s"' % filepath, file=sys.stderr)
@@ -23,10 +23,18 @@ marc_tags = []
 for marc_record in reader:
     csv_record = {}
     for marc_field in marc_record.fields:
-        if marc_field.tag not in marc_tags:
-            marc_tags.append(marc_field.tag)
-        csv_record[marc_field.tag] = marc_field.value()
-    csv_records.append(csv_record)
+        if marc_record is None:
+            print(
+                "Current chunk: ",
+                reader.current_chunk,
+                " was ignored because the following exception raised: ",
+                reader.current_exception
+            )
+        else:
+            if marc_field.tag not in marc_tags:
+                marc_tags.append(marc_field.tag)
+            csv_record[marc_field.tag] = marc_field.value()
+        csv_records.append(csv_record)
 
 marc_tags.sort()
 
